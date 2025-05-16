7289411c784bdd7e47aa472c6b1bf3bb3f744d73
dennis-kim1 <dennis.kim.1@pandarg.com>
2025-05-16 15:31:07 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,6 @@ variable "vsphere_server" {}
 variable "datacenter" {}
 variable "vm_name" {}
 variable "template" {}
-variable "datacenter_id" {}
-variable "resource_pool_id" {}
-variable "datastore_id" {}
 variable "network_id" {}
 variable "num_cpus" { default = 2 }
 variable "memory"   { default = 4096 }

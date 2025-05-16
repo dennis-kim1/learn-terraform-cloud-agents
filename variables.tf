e94731ae287a4165b2884d810446d92e01a60190
dennis-kim1 <dennis.kim.1@pandarg.com>
2025-05-16 15:37:28 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,3 @@ variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
 variable "datacenter" {}
-variable "vm_name" {}
-variable "template" {}
-variable "network_id" {}
-variable "num_cpus" { default = 2 }
-variable "memory"   { default = 4096 }
-variable "disk_size" { default = 40 }
-variable "ip_address" {}
-variable "netmask"    { default = 24 }
-variable "gateway"    {}
-variable "domain"     { default = "local" }

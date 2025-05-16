744f1614cfcfc010088ef239346395a70dde58a3
bernardok-pandarg <bernardo.kummerfeld@pandarg.com>
2025-05-16 14:44:35 -0700

    Update main.tf
    removed   allow_unverified_ssl = true

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {

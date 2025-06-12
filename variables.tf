e4767c11a9bef266d0bbc708b9ea280afab92ee8
dennis-kim1 <dennis.kim.1@pandarg.com>
2025-06-12 10:36:08 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
 variable "datacenter" {}
+variable "datastore" {}
+variable "network" {}
+variable "cluster" {}
+variable "template" {}

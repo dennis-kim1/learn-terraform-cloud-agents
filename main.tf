277799f3e8e430c0ddae48b40e9c18406d757d18
dennis-kim1 <dennis.kim.1@pandarg.com>
2025-06-13 12:55:20 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "vsphere_datastore" "datastore" {
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = replace(var.cluster, "/", "\\/")
+  name          = var.cluster
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 

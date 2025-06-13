52dbdc49f3f944d5fcc4866a1e35227328260749
dennis-kim1 <dennis.kim.1@pandarg.com>
2025-06-13 13:36:15 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,10 @@ variable "vsphere_password" {}
 variable "vsphere_server" {}
 variable "datacenter" {}
 variable "datastore" {}
-variable "cluster" {}
+variable "cluster" {
+  description = "Logical cluster name (key for cluster_map)"
+  type        = string
+}
 variable "network" {}
 variable "template" {}
 variable "vm_name" {}
